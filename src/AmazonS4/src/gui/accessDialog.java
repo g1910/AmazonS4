871d2185f27118126437fdd89437ff932e624427
Gaurav <gaurav.mittal.191013@gmail.com>
2015-03-11 22:30:01 +0530

    Add dialog box to input access keys

---
+++ src/AmazonS4/src/gui/accessDialog.java
@@ -0,0 +1,71 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+
+public class accessDialog extends JDialog {
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					accessDialog dialog = new accessDialog();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public accessDialog() {
+		setTitle("Connect Using New Keys");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JPanel panel = new JPanel();
+		getContentPane().add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		
+		JLabel lblNewLabel = new JLabel("Access Key");
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		getContentPane().add(panel_1);
+		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
+		
+		JLabel lblNewLabel_1 = new JLabel("Secret Access Key");
+		panel_1.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		panel_1.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Connect");
+		btnNewButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		getContentPane().add(btnNewButton);
+
+	}
+}
