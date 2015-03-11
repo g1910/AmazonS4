a42f3561b61f66a1c435fe681d03268e3c5ed489
Gaurav <gaurav.mittal.191013@gmail.com>
2015-03-11 23:35:54 +0530

    Modify MainGui to add cardLayout

--- src/AmazonS4/src/gui/MainGui.java
+++ src/AmazonS4/src/gui/MainGui.java
@@ -26,13 +26,24 @@ import javax.swing.JList;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.Properties;
+import java.awt.CardLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Component;
+
+import javax.swing.SwingConstants;
+
+import utils.AwS3Conn;
+import utils.ConfigManager;
 
 public class MainGui extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField_2;
 	private JTextField textField_3;
-	private JTextArea txtrStatus;
+	private static JTextArea txtrStatus;
+	private AwS3Conn awsS4 = null;
 
 	/**
 	 * Launch the application.
@@ -63,50 +74,37 @@ public class MainGui extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.NORTH);
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		
-		JPanel s3Connect = new JPanel();
-		panel.add(s3Connect);
-		s3Connect.setLayout(new BoxLayout(s3Connect, BoxLayout.X_AXIS));
-		
-		JButton defaultConnBtn = new JButton("Connect Using Default Keys");
-		
-		s3Connect.add(defaultConnBtn);
-		
-		JButton newConnBtn = new JButton("Connect Using Existing Keys");
-		s3Connect.add(newConnBtn);
-		
-		JPanel panel_3 = new JPanel();
-		panel.add(panel_3);
-		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
-		
-		JLabel lblBucketName = new JLabel("Bucket Name");
-		panel_3.add(lblBucketName);
-		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		panel_3.add(textField_2);
-		
-		JButton btnConnect = new JButton("Connect");
-		panel.add(btnConnect);
-		
-		JPanel panel_4 = new JPanel();
-		panel.add(panel_4);
-		panel_4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		JButton btnNewButton = new JButton("Choose Existing Key Pair");
-		panel_4.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Create New Key Pair");
-		panel_4.add(btnNewButton_1);
-		
 		txtrStatus = new JTextArea();
 		contentPane.add(txtrStatus, BorderLayout.SOUTH);
 		
+		JPanel navPanel = new JPanel();
+		contentPane.add(navPanel, BorderLayout.WEST);
+		navPanel.setLayout(new CardLayout(0, 0));
+		
+		JPanel accessPanel = new JPanel();
+		navPanel.add(accessPanel, "name_16056552804186");
+		
+		JPanel s3Connect = new JPanel();
+		accessPanel.add(s3Connect);
+		
+		JButton defaultConnBtn = new JButton("Connect Using Default Keys");
+		defaultConnBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		s3Connect.setLayout(new BorderLayout(0, 0));
+		
+		s3Connect.add(defaultConnBtn, BorderLayout.NORTH);
+		
+		JPanel panel_1 = new JPanel();
+		s3Connect.add(panel_1);
+		
+		JPanel s4Panel = new JPanel();
+		navPanel.add(s4Panel, "name_16129026029748");
+		s4Panel.setLayout(new BorderLayout(0, 0));
+		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		contentPane.add(tabbedPane, BorderLayout.CENTER);
+		s4Panel.add(tabbedPane);
 		
 		JPanel panel_5 = new JPanel();
 		tabbedPane.addTab("Upload", null, panel_5, null);
@@ -162,16 +160,46 @@ public class MainGui extends JFrame {
 		JList list = new JList();
 		panel_6.add(list);
 		
+		JPanel panel = new JPanel();
+		s4Panel.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3);
+		panel_3.setLayout(new BoxLayout(panel_3, BoxLayout.X_AXIS));
+		
+		JLabel lblBucketName = new JLabel("Bucket Name");
+		panel_3.add(lblBucketName);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		panel_3.add(textField_2);
+		
+		JPanel panel_4 = new JPanel();
+		panel_3.add(panel_4);
+		panel_4.setLayout(new BoxLayout(panel_4, BoxLayout.X_AXIS));
+		
+		JButton btnNewButton = new JButton("Choose Existing Key Pair");
+		panel_4.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Create New Key Pair");
+		panel_4.add(btnNewButton_1);
+		
 		defaultConnBtn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				log("Using Default Keys to connect to AWS S3");
+				Properties prop = ConfigManager.getProperties();
+				if(prop!=null){
+					awsS4 = new AwS3Conn(prop.getProperty("access-key"), prop.getProperty("secret-access-key"));
+					log("Connected to AWS S3");
+				}
 				
 			}
 		});
 	}
 	
-	public void log(String msg){
+	public static void log(String msg){
 		txtrStatus.append(msg+"\n");
 	}
 }
