d55a77a262b4fde4c4c110ff34f9faa68bd9bb95
Gaurav <gaurav.mittal.191013@gmail.com>
2015-03-11 23:34:12 +0530

    Add functionality to store and retrieve configuration

---
+++ src/AmazonS4/src/utils/ConfigManager.java
@@ -0,0 +1,42 @@
+package utils;
+
+import gui.MainGui;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ConfigManager {
+	public static Properties getProperties(){
+		Properties prop = new Properties();
+		String filename = "config.properties";
+
+		InputStream input = null;
+		input = ConfigManager.class.getClassLoader()
+				.getResourceAsStream(filename);
+		
+		if (input == null) {
+			MainGui.log("Sorry, unable to find " + filename + "\nTry connecting using new Access Keys");
+
+		} else {
+			//input from config.properties is valid
+			//establishing connection with the database
+			try {
+				//loading the properties
+				prop.load(input);
+				//System.out.println(prop.getProperty("ipport"));
+				//System.out.println(prop.getProperty("database"));
+				//System.out.println(prop.getProperty("user"));
+				//System.out.println(prop.getProperty("password"));
+				
+				return prop;
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		return null;
+	}
+}
