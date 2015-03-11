bc6ea3e87f574c972894f39f4f126e3b1dcc7200
Gaurav <gaurav.mittal.191013@gmail.com>
2015-03-12 03:15:48 +0530

    Add object listing functionality

--- src/AmazonS4/src/utils/AwS3Conn.java
+++ src/AmazonS4/src/utils/AwS3Conn.java
@@ -1,21 +1,28 @@
 package utils;
 
+import gui.MainGui;
+
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.transfer.TransferManager;
 
 public class AwS3Conn {
 	AWSCredentials credentials;
 	AmazonS3 s3client;
 	String currBucketName;
-	
-	public AwS3Conn(String accessKey, String secretAccessKey){
-		credentials = new BasicAWSCredentials(accessKey,secretAccessKey);
+	ObjectListing objListing;
+
+	public AwS3Conn(String accessKey, String secretAccessKey) {
+		credentials = new BasicAWSCredentials(accessKey, secretAccessKey);
 		s3client = new AmazonS3Client(credentials);
 	}
-	
-	public void createBucket(String name){
+
+	public void createBucket(String name) {
 		s3client.createBucket(name);
 	}
 
@@ -33,5 +40,39 @@ public class AwS3Conn {
 
 	public void setS3client(AmazonS3 s3client) {
 		this.s3client = s3client;
+	}
+
+	public void setBucket(String name) {
+		MainGui.log("Checking if bucket "+name+ " exists...");
+		if (s3client.doesBucketExist(name)) {
+			MainGui.log("Bucket "+name+ " already exists...");
+		} else {
+			MainGui.log("Bucket "+name + " does not exist!\nCreating bucket...");
+			createBucket(name);
+			MainGui.log("Created bucket " + name);
+		}
+		MainGui.log("Setting bucket to "+name);
+		currBucketName = name;
+	}
+	
+	public String getBucket(){
+		return currBucketName;
+	}
+	
+	public ObjectListing getObjectListing(String prefix){
+		ListObjectsRequest listObjectsRequest = new ListObjectsRequest()
+        .withBucketName(currBucketName)
+        .withPrefix(prefix);
+		do {
+            objListing = s3client.listObjects(listObjectsRequest);
+            for (S3ObjectSummary objectSummary : 
+            	objListing.getObjectSummaries()) {
+                System.out.println(" - " + objectSummary.getKey() + "  " +
+                        "(size = " + objectSummary.getSize() + 
+                        ")");
+            }
+            listObjectsRequest.setMarker(objListing.getNextMarker());
+        } while (objListing.isTruncated());
+		return objListing;
 	}
 }
